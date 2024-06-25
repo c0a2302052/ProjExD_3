6b0519aee7a21d5ebf047dd4287b7b8a021bfd3f
nagga <c0a2302052@edu.teu.ac.jp>
2024-06-25 14:46:51 +0900

    練習3 喜ぶこうかとんの実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -174,6 +174,7 @@ def main():
                     # syoutotuhantei
                     bomb = None
                     beam = None
+                    bird.change_img(6, screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
