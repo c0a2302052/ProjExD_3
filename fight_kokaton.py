e1dbbfa7787422eefe0111285477728a42a88cb7
nagga <c0a2302052@edu.teu.ac.jp>
2024-06-25 14:53:32 +0900

    練習4 gameover画面の実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -160,13 +160,13 @@ def main():
         
         
         if bomb is not None:
-            if beam is not None:
-                if bird.rct.colliderect(bomb.rct):
-                    # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                    bird.change_img(8, screen)
-                    pg.display.update()
-                    time.sleep(1)
-                    return
+            if bird.rct.colliderect(bomb.rct):
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
+                pg.display.update()
+                time.sleep(5)
+                return
         
         if bomb is not None:
             if beam is not None:
